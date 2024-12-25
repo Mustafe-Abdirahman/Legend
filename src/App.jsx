69de4b04eec4c7d2b18acd4658d9a61aca477f18
Mustafe-Abdirahman <khadar3021@gmail.com>
2024-12-25 08:31:36 +0300

    Upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Service from "./pages/Services";
+import Service from "./pages/Service";
 import Price from "./pages/Price";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/" element={<Service />} />
+
         <Route path="/" element={<Price />} />
       </Routes>
       <Header />
